fc191e0d48feb876883bcf72c591877f930b8e53
dougfort <doug.fort@gmail.com>
2022-08-24 13:25:51 -0400

    implement enough to work the wikipedia example

--- src/lib.rs
+++ src/lib.rs
@@ -1,39 +1,52 @@
 pub trait State<T> {
     fn curr(&self) -> T;
-    fn next(&self, input: T) -> Option<Self> where Self: std::marker::Sized;
+    fn next(&self, input: T) -> Self;
 }
-
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     struct UsizeState {
-        curr: u8,        
+        curr: u8,
+        funcs: [fn(u8) -> u8; 3],
     }
 
     impl UsizeState {
-        fn new(curr: u8) -> Self {
-            UsizeState { curr }
+        fn new(curr: u8, funcs: [fn(u8) -> u8; 3]) -> Self {
+            UsizeState { curr, funcs }
         }
     }
 
     impl State<u8> for UsizeState {
-        fn next(&self, input: u8) -> Option<Self> {
-            Some(UsizeState::new(self.curr + input))
+        fn next(&self, input: u8) -> Self {
+            UsizeState::new((self.funcs[self.curr as usize])(input), self.funcs)
         }
-        fn curr (&self) -> u8 {
-            self.curr            
+        fn curr(&self) -> u8 {
+            self.curr
         }
-    }  
+    }
 
     #[test]
-    fn usize_next() {
-        let u1 = UsizeState::new(5);
-        let result = u1.next(1);
-        assert!(result.is_some());
-        if let Some(state) = result {
-            assert_eq!(state.curr(), 6);
-        }
+    fn wikipedia_example() {
+        // https://en.wikipedia.org/wiki/Deterministic_finite_automaton
+        let funcs: [fn(u8) -> u8; 3] = [
+            |x| match x {
+                0 => 0,
+                _ => 1,
+            },
+            |x| match x {
+                0 => 2,
+                _ => 0,
+            },
+            |x| match x {
+                0 => 1,
+                _ => 2,
+            },
+        ];
+
+        let s = UsizeState::new(0, funcs);
+        let result = [1u8, 0u8, 0u8, 1u8].iter().fold(s, |s, x| s.next(*x));
+        assert_eq!(result.curr(), 0);
     }
 }
