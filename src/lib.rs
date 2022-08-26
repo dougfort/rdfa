f2d1ba3645a2771c0eaed55a567834a019c9ac53
dougfort <doug.fort@gmail.com>
2022-08-26 10:45:13 -0400

    Make state funcs global const

--- src/lib.rs
+++ src/lib.rs
@@ -7,20 +7,34 @@ pub trait State<T> {
 mod tests {
     use super::*;
 
+    const FUNCS: [fn(u8) -> u8; 3] = [
+        |x| match x {
+            0 => 0,
+            _ => 1,
+        },
+        |x| match x {
+            0 => 2,
+            _ => 0,
+        },
+        |x| match x {
+            0 => 1,
+            _ => 2,
+        },
+    ];
+
     struct UsizeState {
         curr: u8,
-        funcs: [fn(u8) -> u8; 3],
     }
 
     impl UsizeState {
-        fn new(curr: u8, funcs: [fn(u8) -> u8; 3]) -> Self {
-            UsizeState { curr, funcs }
+        fn new(curr: u8) -> Self {
+            UsizeState { curr }
         }
     }
 
     impl State<u8> for UsizeState {
         fn next(&self, input: u8) -> Self {
-            UsizeState::new((self.funcs[self.curr as usize])(input), self.funcs)
+            UsizeState::new((FUNCS[self.curr as usize])(input))
         }
         fn curr(&self) -> u8 {
             self.curr
@@ -29,25 +43,9 @@ mod tests {
 
     #[test]
     fn wikipedia_example() {
-        // https://en.wikipedia.org/wiki/Deterministic_finite_automaton
-        let funcs: [fn(u8) -> u8; 3] = [
-            |x| match x {
-                0 => 0,
-                _ => 1,
-            },
-            |x| match x {
-                0 => 2,
-                _ => 0,
-            },
-            |x| match x {
-                0 => 1,
-                _ => 2,
-            },
-        ];
-
         let result = [1u8, 0u8, 0u8, 1u8]
             .iter()
-            .fold(UsizeState::new(0, funcs), |s, x| s.next(*x));
+            .fold(UsizeState::new(0), |s, x| s.next(*x));
         assert_eq!(result.curr(), 0);
     }
 }
